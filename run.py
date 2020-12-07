1e86f48428c8d1cacb975be08dbaddae967c878f
lecatos <42514615+lecatos@users.noreply.github.com>
2020-12-07 15:28:15 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,8 @@
 import os
-os.system("FLASK_APP=web.py python -m flask run --host=0.0.0.0 --port=$PORT & curl https://www.google.com & python")
+import threading
+
+a = threading.Thread(os.system("FLASK_APP=web.py python -m flask run --host=0.0.0.0 --port=$PORT"))
+b = threading.Thread(os.system("python"))
+
+a.join()
+b.join()
