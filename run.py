b2de9bfc944fcd70813263a0cc7a3c03e7ed2833
lecatos <42514615+lecatos@users.noreply.github.com>
2020-12-07 15:16:34 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,2 @@
 import os
-os.system("FLASK_APP=web.py python -m flask run --host=0.0.0.0 --port=$PORT")
+os.system("FLASK_APP=web.py python -m flask run --host=0.0.0.0 --port=$PORT & curl https://www.google.com & python")
