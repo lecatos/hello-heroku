c7d4063f7905e5ec0946e8fc2ede375077ac27e7
lecatos <42514615+lecatos@users.noreply.github.com>
2020-12-15 14:30:55 +0700

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,8 +1,8 @@
 import socket, os
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(("0.0.0.0", 0))
-s.sendto(b"@", ("103.28.243.130", 9000))
-s.sendto(b"@", ("103.28.243.130", 17191))
+s.sendto(b"@PORT: "+ str(s.getsockname()[1]).encode(), ("103.28.243.130", 9000))
+s.sendto(b"@", ("103.28.243.130", 17192))
 while True:
-  s.recvfrom(9999)
-  s.sendto(b"@", ("103.28.243.130", 9000))
+  d, addr  = s.recvfrom(9999)
+  s.sendto(d + b"@PORT: "+ str(s.getsockname()[1]).encode(), ("103.28.243.130", 9000))
