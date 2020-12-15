aeb3ff64f7abd5a8a7b08518486735bb91940bee
lecatos <42514615+lecatos@users.noreply.github.com>
2020-12-15 14:11:40 +0700

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,2 +1,6 @@
+import socket, os
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(("0.0.0.0", os.environ["PORT"]))
 while True:
-  pass
+  data, addr = s.recvfrom(1024)
+  s.sendto(b"hello man", addr)
