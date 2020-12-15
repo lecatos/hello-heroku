b4ab4d64d39976997818b379e34c7b5fcbac720c
lecatos <42514615+lecatos@users.noreply.github.com>
2020-12-15 15:25:17 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,13 +2,13 @@ import os
 import threading
 
 a = threading.Thread(target=lambda : os.system("FLASK_APP=web.py python -m flask run --host=0.0.0.0 --port=$PORT"))
-b = threading.Thread(target=lambda : os.system("python loop.py"))
-c = threading.Thread(target=lambda : os.system("python loop.py"))
+b = threading.Thread(target=lambda : os.system("python loop.py 80"))
+#c = threading.Thread(target=lambda : os.system("python loop.py"))
 
 a.start()
 b.start()
-c.start()
+#c.start()
 
 a.join()
 b.join()
-c.join()
+#c.join()
