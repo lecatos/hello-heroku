e9aaba52541310213ce39db6a063dc7b3efc25ce
lecatos <42514615+lecatos@users.noreply.github.com>
2020-12-15 15:30:33 +0700

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,6 +1,6 @@
 import socket, os ,sys
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(("0.0.0.0", str(sys.argv[1])))
+s.bind(("0.0.0.0", int(sys.argv[1])))
 s.sendto(b"@PORT: "+ str(s.getsockname()[1]).encode(), ("103.28.243.130", 9000))
 s.sendto(b"@", ("103.28.243.130", 17192))
 while True:
