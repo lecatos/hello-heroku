c1dcce7f72334e56b0bbf67619105ef587e7f8a9
lecatos <42514615+lecatos@users.noreply.github.com>
2020-12-15 15:32:36 +0700

    Update web.py

--- web.py
+++ web.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return str( [(int(p), c) for p, c in [x.rstrip('\n').split(' ', 1) for x in os.popen('ps h -eo pid:1,command')]] ) + "build api"
+  return str( [(int(p), c) for p, c in [x.rstrip('\n').split(' ', 1) for x in os.popen('ps h -eo pid:1,command')]] ) + os.environ["PORT]
